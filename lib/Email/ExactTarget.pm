1f0b1dbf100c05c8a35d5cc4df9407dfd78c13b2
Guillaume Aubert <aubertg@cpan.org>
2012-05-29 22:46:20 -0400

    Released version 1.3.1.

--- lib/Email/ExactTarget.pm
+++ lib/Email/ExactTarget.pm
@@ -24,11 +24,11 @@ Email::ExactTarget - Interface to ExactTarget's API.
 
 =head1 VERSION
 
-Version 1.3.0
+Version 1.3.1
 
 =cut
 
-our $VERSION = '1.3.0';
+our $VERSION = '1.3.1';
 
 our $ENDPOINT_LIVE = 'https://webservice.exacttarget.com/Service.asmx';
 
