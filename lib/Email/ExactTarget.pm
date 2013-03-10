ab331c5955e9ad9750cc3e86841933f290806c03
Guillaume Aubert <aubertg@cpan.org>
2013-03-10 16:12:36 -0400

    Updated copyright year.

--- lib/Email/ExactTarget.pm
+++ lib/Email/ExactTarget.pm
@@ -542,7 +542,7 @@ and write code for them!
 
 =head1 COPYRIGHT & LICENSE
 
-Copyright 2009-2012 Guillaume Aubert.
+Copyright 2009-2013 Guillaume Aubert.
 
 This program is free software: you can redistribute it and/or modify it under
 the terms of the GNU General Public License version 3 as published by the Free
